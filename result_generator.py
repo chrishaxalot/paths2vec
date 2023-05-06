f98112f23dcc16716b4f6504a6e0da6a89821aed
chrishaxalot <45143330+chrishaxalot@users.noreply.github.com>
2023-05-06 23:26:05 +0200

    moved line into main block

--- result_generator.py
+++ result_generator.py
@@ -5,24 +5,23 @@ from helpers.pipeline_evaluator import PipelineEvaluator
 
 import gensim
 
-print(f"are you using fast gensim?: {gensim.models.doc2vec.FAST_VERSION > -1}")
 
 cpu_count = 16
-num_runs = 3
+num_runs = 10
 sample_size = 1000
 window_in_nodes = 3
 
 dataset_estimator_dict = {
     # (multi-task) binary classification
     # "ogbg-molhiv": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molpcba": MultiOutputClassifier(svm.SVC()),
-    "ogbg-moltox21": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molbace": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molbbbp": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molclintox": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molmuv": MultiOutputClassifier(svm.SVC()),
-    "ogbg-molsider": MultiOutputClassifier(svm.SVC()),
-    "ogbg-moltoxcast": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molpcba": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-moltox21": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molbace": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molbbbp": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molclintox": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molmuv": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-molsider": MultiOutputClassifier(svm.SVC()),
+    # "ogbg-moltoxcast": MultiOutputClassifier(svm.SVC()),
     # regression
     "ogbg-molesol": MultiOutputRegressor(svm.SVR()),
     "ogbg-molfreesolv": MultiOutputRegressor(svm.SVR()),
@@ -30,6 +29,8 @@ dataset_estimator_dict = {
 }
 
 if __name__ == "__main__":
+    print(f"are you using fast gensim?: {gensim.models.doc2vec.FAST_VERSION > -1}")
+
     pipeline_evaluator = PipelineEvaluator(
         cpu_count=cpu_count,
         num_runs=num_runs,
